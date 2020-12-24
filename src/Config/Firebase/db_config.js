c21453b1a20064c015305b45f55039f3859160bc
trung3120 <trunghuynh3010@gmail.com>
2020-12-24 11:42:39 +0700

    first commit

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+        apiKey: "AIzaSyCgx8GwdSN1W9KSuCFkcfQaPvtq7G8s79M",
+        authDomain: "react-flashcards-435cd.firebaseapp.com",
+        projectId: "react-flashcards-435cd",
+        storageBucket: "react-flashcards-435cd.appspot.com",
+        messagingSenderId: "1020778850833",
+        appId: "1:1020778850833:web:955626901f77ea8f87dfb0",
+        measurementId: "G-FV08KXC1N6"
+}
